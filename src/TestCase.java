27f29066f8779041e7d4333bf5bc86f4cea5a5ac
zhuqiao1018 <zqno007@qq.com>
2015-08-15 18:36:45 +0800

    compare version

--- src/TestCase.java
+++ src/TestCase.java
@@ -2,14 +2,14 @@
  * Created by ruby on 2015/8/14.
  */
 
-import solutions.string.MyAtoi;
+import solutions.string.AddBinary;
 
 
 public class TestCase {
     public static void main(String[] args) {
-        MyAtoi test = new MyAtoi();
+        AddBinary test = new AddBinary();
 
-        System.out.println(test.myAtoi("344.5"));
+        System.out.println(test.compareVersion("1", "1.01.0"));
 
     }
 }
--- src/solutions/string/AddBinary.java
+++ src/solutions/string/AddBinary.java
@@ -4,4 +4,29 @@ package solutions.string;
  * Created by ruby on 2015/8/14.
  */
 public class AddBinary {
+    public int compareVersion(String version1, String version2) {
+        String[] v1;
+        String[] v2;
+        v1 = version1.split("\\.");
+        v2 = version2.split("\\.");
+
+        int count = 0;
+        while(count < v1.length || count < v2.length) {
+            if(count == v1.length && count == v2.length)
+                return 0;
+            if(count == v1.length) {
+                return Integer.parseInt(v2[count]) == 0 ? 0 : -1;
+            }
+            if(count == v2.length) {
+                return Integer.parseInt(v1[count]) == 0 ? 0 : 1;
+            }
+            if(Integer.parseInt(v1[count]) > Integer.parseInt(v2[count]))
+                return  1;
+            if(Integer.parseInt(v1[count]) < Integer.parseInt(v2[count]))
+                return -1;
+            count ++;
+        }
+
+        return 0;
+    }
 }
