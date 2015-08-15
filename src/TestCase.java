9e6552a90e43271c4a20fe5a503ea9c9d8b747b0
zhuqiao1018 <zqno007@qq.com>
2015-08-15 19:13:18 +0800

    init countAndSay

--- src/TestCase.java
+++ src/TestCase.java
@@ -2,14 +2,14 @@
  * Created by ruby on 2015/8/14.
  */
 
-import solutions.string.AddBinary;
+import solutions.string.CountAndSay;
 
 
 public class TestCase {
     public static void main(String[] args) {
-        AddBinary test = new AddBinary();
+        CountAndSay test = new CountAndSay();
 
-        System.out.println(test.compareVersion("1", "1.01.0"));
+        System.out.println(test.countAndSay(4));
 
     }
 }
---
+++ src/solutions/string/CountAndSay.java
@@ -0,0 +1,11 @@
+package solutions.string;
+
+/**
+ * Created by ruby on 2015/8/15.
+ */
+public class CountAndSay {
+    public String countAndSay(int n) {
+        
+        return "";
+    }
+}
