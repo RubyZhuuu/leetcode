7a6e77ac5d85c864f9f69cd0c88eb819856d07be
zhuqiao1018 <zqno007@qq.com>
2015-08-16 12:30:03 +0800

    countAndSay

--- src/solutions/string/CountAndSay.java
+++ src/solutions/string/CountAndSay.java
@@ -5,7 +5,29 @@ package solutions.string;
  */
 public class CountAndSay {
     public String countAndSay(int n) {
-        
-        return "";
+        if(n == 1)
+            return "1";
+
+        String sequence = countAndSay(n-1);
+        StringBuffer strBuffer = new StringBuffer();
+
+        char temp = sequence.charAt(0);
+        int nums = 1;
+
+        for(int index = 1; index < sequence.length(); index ++) {
+            if(sequence.charAt(index) == temp) {
+                nums ++;
+                continue;
+            } else {
+                strBuffer.append(nums);
+                strBuffer.append(temp);
+                temp = sequence.charAt(index);
+                nums = 1;
+            }
+        }
+        strBuffer.append(nums);
+        strBuffer.append(temp);
+
+        return strBuffer.toString();
     }
 }
