19c6b3d07e265734b20d219a71f746c70b3a9f35
zhuqiao1018 <zqno007@qq.com>
2015-09-06 18:30:40 +0800

    testCase

--- src/TestCase.java
+++ src/TestCase.java
@@ -2,18 +2,19 @@
  * Created by ruby on 2015/8/14.
  */
 
-import array.RemoveElement;
+import math.AddDigits;
+import array.medium.TwoSum;
+
+import java.util.List;
 
 
 public class TestCase {
     public static void main(String[] args) {
-        RemoveElement test = new RemoveElement();
-        int[] a= {3,7,6,3,5, 3, 3};
-        int afterLength = test.removeElement(a, 8);
+        TwoSum test = new TwoSum();
+        int[] a1= {4,5,2,7,4};
+        int[] a2 = test.twoSum(a1, 15);
 
-        System.out.println("after:" + afterLength);
-        for(int temp : a) {
-            System.out.println(temp);
-        }
+        System.out.println(a2[0]);
+        System.out.println(a2[1]);
     }
 }
