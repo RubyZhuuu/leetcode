ba215902e05f825248fcdf5d51a8f089bfe9e9f1
zhuqiao1018 <zqno007@qq.com>
2015-09-28 21:35:52 +0800

    BinaryTreePahts

--- src/TestCase.java
+++ src/TestCase.java
@@ -10,15 +10,43 @@ import dynamicProgramming.ClimbingStairs;
 
 
 import java.util.List;
+import java.util.Scanner;
 
 
 public class TestCase {
 
-    public static void main(String[] args) {
+    //        ClimbingStairs test = new ClimbingStairs();
+//        MinimunSizeSubarraySum t = new MinimunSizeSubarraySum();
+//        int[] a= {1,3,4};
+//        System.out.println(test.climbStairs(4));
 
-        ClimbingStairs test = new ClimbingStairs();
-        MinimunSizeSubarraySum t = new MinimunSizeSubarraySum();
-        int[] a= {1,3,4};
-        System.out.println(test.climbStairs(4));
+    public int getPath() {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int nums = sc.nextInt();
+
+        int[] path = new int[nums + 1];
+
+        for(int i = 0; i <= nums ; i++) {
+            path[i] = sc.nextInt();
+        }
+
+        int remainder = 7;
+        int count = 0;
+        int j = 0;
+
+        while(j < nums) {
+            remainder -= path[j];
+
+            if(remainder >= path[j + 1])
+                break;
+            remainder = 7;
+            count ++;
+            j++;
+        }
+
+        return count;
     }
 }
