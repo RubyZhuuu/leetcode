0457ca781f0007e457ab398c051cca0de439c0c2
Zhu Qiao <zqno007@qq.com>
2015-11-03 17:28:56 +0800

    edit gitignore && two array problems

---
+++ src/array/ThreeSum.java
@@ -0,0 +1,20 @@
+package array;
+
+import java.util.*;
+
+/**
+ * no. 15
+ * Created by Ruby on 2015/10/29 15:59.
+ * Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0?
+ * Find all unique triplets in the array which gives the sum of zero.
+ * Note:
+ * Elements in a triplet (a,b,c) must be in non-descending order. (ie, a ¡Ü b ¡Ü c)
+ * The solution set must not contain duplicate triplets.
+ */
+public class ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new LinkedList<>();
+
+        return res;
+    }
+}
