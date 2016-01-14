10788b4263fbf3d17ff3879aff5ae0d59be130b3
Zhu Qiao <zqno007@qq.com>
2016-01-14 16:49:16 +0800

    linkedlist

--- src/linkedList/ReverseLinkedList2.java
+++ src/linkedList/ReverseLinkedList2.java
@@ -9,4 +9,28 @@ package linkedList;
  * Created by Ruby on 2016/1/6 13:58.
  */
 public class ReverseLinkedList2 {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        ListNode dummy = new ListNode(0), tail, start;
+        dummy.next = head;
+        start = dummy;
+
+        int num = n - m + 1;
+        while(head != null && --m > 0) {
+            head = head.next;
+            start = start.next;
+        }
+        tail = head;
+
+        while(head != null && num -- > 0) {
+            ListNode temp = start.next;
+            start.next = head;
+            head = head.next;
+            start.next.next = temp;
+        }
+        if(tail != head)
+            tail.next = head;
+
+        return dummy.next;
+
+    }
 }
